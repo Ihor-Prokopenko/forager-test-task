ebcd2b228cb6296291af89e2b6b267be95dfc0a4
Ihor Prokopenko <swaghettiemonsta@gmail.com>
2024-01-06 16:10:24 +0200

    corrected __init__.py in module

--- weather_client/__init__.py
+++ weather_client/__init__.py
@@ -1,9 +1,3 @@
 """Module providing weather-related functionality."""
 from .weather_service import WeatherService
 from .weather_client import WeatherAPIClient, WeatherResult
-
-__all__ = [
-    'WeatherAPIClient',
-    'WeatherResult',
-    'WeatherService',
-]
